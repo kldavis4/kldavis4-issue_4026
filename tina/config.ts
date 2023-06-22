86a2a9bfa65523f09269bb7499481b2ee8108ef3
Joachim Beck <beck@tms-institut.de>
2023-06-22 14:00:30 +0200

    initial commit

---
+++ tina/config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  clientId: process.env.TINA_CLIENT_ID!,
+  branch: branch,
+  token: process.env.TINA_TOKEN!,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "static",
+    },
+  },
+  search: {
+    tina: {
+      indexerToken: process.env.TINA_SEARCH_TOKEN!,
+      stopwordLanguages: ['deu'],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            label: "Date",
+            name: "date",
+          },
+          {
+            type: "boolean",
+            name: "draft",
+            label: "Draft",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
